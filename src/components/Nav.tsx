96d30290c66cc2b42c83eab2ae6fe37174d7852c
bunhancodeinghub <sakanp@iottechgroup.com>
2023-10-03 10:31:06 +0700

    ลองแก้

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -76,7 +76,7 @@ const NaV: React.FC = () => {
           </a>{" "}
           |
           <a style={{ color: "black" }} href="/Performance">
-            Performance
+            PerformanceXX
           </a>
         </div>
         <div className="buttonmenu">
